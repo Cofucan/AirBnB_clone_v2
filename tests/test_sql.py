1bc8a4b12b7d30415ef5db5c5212b70a1affa3b7
Uche Ofia <coolucheofia@gmail.com>
2023-08-24 02:13:55 +0100

    style: PEP8 compliant

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,18 +1,21 @@
 #!/usr/bin/pyhthon3
 """Module for the test of MySQL"""
-import MySQLdb
-import unittest
-from unittest.mock import patch
 import io
-from console import HBNBCommand
-from os import getenv
-from models.engine.db_storage import DBStorage
 import os
+import unittest
+from os import getenv
+from unittest.mock import patch
+
+import MySQLdb
+
+from console import HBNBCommand
+from models.engine.db_storage import DBStorage
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "Not DBStorage")
+@unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "Not DBStorage")
 class TestMySQL(unittest.TestCase):
     """Test for the SQL database"""
+
     conn = None
     cur = None
 
@@ -20,10 +23,12 @@ class TestMySQL(unittest.TestCase):
         """Connect to MySQLdb"""
         storage = DBStorage()
         storage.reload()
-        self.conn = MySQLdb.connect(getenv('HBNB_MYSQL_HOST'),
-                                    getenv('HBNB_MYSQL_USER'),
-                                    getenv('HBNB_MYSQL_PWD'),
-                                    getenv('HBNB_MYSQL_DB'))
+        self.conn = MySQLdb.connect(
+            getenv("HBNB_MYSQL_HOST"),
+            getenv("HBNB_MYSQL_USER"),
+            getenv("HBNB_MYSQL_PWD"),
+            getenv("HBNB_MYSQL_DB"),
+        )
         self.cur = self.conn.cursor()
 
     def disconnection(self):
@@ -36,7 +41,7 @@ class TestMySQL(unittest.TestCase):
     def test_create_state(self):
         """Test create of a State"""
         self.connection()
-        with patch('sys.stdout', new=io.StringIO()) as f:
+        with patch("sys.stdout", new=io.StringIO()):
             HBNBCommand().onecmd('create State name="California"')
         self.cur.execute("SELECT COUNT(*) FROM states")
         res = self.cur.fetchone()[0]
@@ -46,17 +51,19 @@ class TestMySQL(unittest.TestCase):
     def test_create_city(self):
         """Test create of a City"""
         self.connection()
-        with patch('sys.stdout', new=io.StringIO()) as f:
+        with patch("sys.stdout", new=io.StringIO()) as file:
             HBNBCommand().onecmd('create State name="California"')
-        id = f.getvalue()[:-1]
-        with patch('sys.stdout', new=io.StringIO()) as f:
-            HBNBCommand().onecmd(f'''create City state_id="{id}"
-                                 name="San_Francisco"''')
+        city_id = file.getvalue()[:-1]
+        with patch("sys.stdout", new=io.StringIO()) as file:
+            HBNBCommand().onecmd(
+                f'''create City state_id="{city_id}"
+                                 name="San_Francisco"'''
+            )
         self.cur.execute("SELECT COUNT(*) FROM cities")
         res = self.cur.fetchone()[0]
         self.assertEqual(res, 1)
         self.disconnection()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
