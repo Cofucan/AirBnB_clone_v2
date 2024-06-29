6d088fbac1b97c5ebe61e7f76b5ed7890c947ffc
StellaGift <giftada151@gmail.com>
2024-06-29 23:15:15 +0000

    modified

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,7 +12,7 @@ from fabric.api import env, put, run, local
 # Web server IPs
 env.hosts = ['34.234.201.201', '52.3.249.208']
 
-# Set the SSH key and username as environment variables
+#Set the SSH key and username as environment variables
 env.key_filename = '~/.ssh/id_rsa'
 env.user = 'ubuntu'
 
